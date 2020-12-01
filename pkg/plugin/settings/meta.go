343b485583e652c49be2f975f5ef2dbd2b61fa78
GuessWhoSamFoo <foos@vmware.com>
2020-12-01 13:23:02 -0800

    Added github actions and tests
    Signed-off-by: GuessWhoSamFoo <foos@vmware.com>

--- pkg/plugin/settings/meta.go
+++ pkg/plugin/settings/meta.go
@@ -6,10 +6,12 @@ const (
 	icon        = "flask"
 )
 
+// GetName returns the plugin name
 func GetName() string {
 	return name
 }
 
+// GetDescription returns the plugin description
 func GetDescription() string {
 	return description
 }
