3ccb6aa88c5cd4e8f501cab9daf9fe951c4ff9d2
Sam Foo <foos@vmware.com>
2021-04-01 11:42:19 -0700

    Move to kind 0.10 and octant 0.18
    Signed-off-by: Sam Foo <foos@vmware.com>

--- pkg/plugin/views/node_images.go
+++ pkg/plugin/views/node_images.go
@@ -14,15 +14,15 @@ type OrderedMap struct {
 func NewImageMap() *OrderedMap {
 	return &OrderedMap{
 		m: map[string]string{
-			"v1.19.1":  "kindest/node:v1.19.1@sha256:98cf5288864662e37115e362b23e4369c8c4a408f99cbc06e58ac30ddc721600",
-			"v1.18.8":  "kindest/node:v1.18.8@sha256:f4bcc97a0ad6e7abaf3f643d890add7efe6ee4ab90baeb374b4f41a4c95567eb",
-			"v1.17.11": "kindest/node:v1.17.11@sha256:5240a7a2c34bf241afb54ac05669f8a46661912eab05705d660971eeb12f6555",
+			"v1.20.2": "kindest/node:v1.20.2@sha256:8f7ea6e7642c0da54f04a7ee10431549c0257315b3a634f6ef2fecaaedb19bab",
+			"v1.19.7": "kindest/node:v1.19.7@sha256:a70639454e97a4b733f9d9b67e12c01f6b0297449d5b9cbbef87473458e26dca",
+			"v1.18.15": "kindest/node:v1.18.15@sha256:5c1b980c4d0e0e8e7eb9f36f7df525d079a96169c8a8f20d8bd108c0d0889cc4",
+			"v1.17.17": "kindest/node:v1.17.17@sha256:7b6369d27eee99c7a85c48ffd60e11412dc3f373658bc59b7f4d530b7056823e",
 			"v1.16.15": "kindest/node:v1.16.15@sha256:a89c771f7de234e6547d43695c7ab047809ffc71a0c3b65aa54eda051c45ed20",
 			"v1.15.12": "kindest/node:v1.15.12@sha256:d9b939055c1e852fe3d86955ee24976cab46cba518abcb8b13ba70917e6547a6",
 			"v1.14.10": "kindest/node:v1.14.10@sha256:ce4355398a704fca68006f8a29f37aafb49f8fc2f64ede3ccd0d9198da910146",
-			"v1.13.12": "kindest/node:v1.13.12@sha256:1c1a48c2bfcbae4d5f4fa4310b5ed10756facad0b7a2ca93c7a4b5bae5db29f5",
 		},
-		keys: []string{"v1.19.1", "v1.18.8", "v1.17.11", "v1.16.15", "v1.15.12", "v1.14.10", "v1.13.12"},
+		keys: []string{"v1.20.2", "v1.19.7", "v1.18.15", "v1.17.17", "v1.16.15", "v1.15.12", "v1.14.10"},
 	}
 }
 
